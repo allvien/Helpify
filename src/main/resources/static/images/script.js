75237bb1374464d8996eab0ef6d4dbc1895eb32a
JonasWeilander <jonas.weilander@gmail.com>
2022-10-18 13:40:32 +0200

    Added donation functions on org-pages

--- src/main/resources/static/images/script.js
+++ src/main/resources/static/images/script.js
@@ -1,6 +1,35 @@
 
 const options = document.querySelector(".options")
+const donationButton = document.querySelector("input.donateButton")
+const orgDonationButton = document.querySelector("input.orgDonateButton")
 
 function quick() {
     options.classList.remove("hide")
+}
+
+function buttonHide() {
+    const sum = document.querySelector('input.donateSum').value;
+    const checkList = document.querySelectorAll('input.donationCheck');
+    donationButton.disabled = true
+    let checkMark = false
+
+    for (let i = 0; i < checkList.length; i++) {
+        if (checkList[i].checked) {
+            checkMark = true
+            break
+        }
+    }
+
+    if (sum > 0 && checkMark) {
+        donationButton.disabled = false
+    }
+}
+
+function orgButtonHide() {
+    const orgSum = document.querySelector('input.orgDonateSum').value;
+    orgDonationButton.disabled = true
+
+    if (orgSum > 0) {
+        orgDonationButton.disabled = false
+    }
 }
