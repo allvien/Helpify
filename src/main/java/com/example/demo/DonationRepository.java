dae1004b6a72cc6efbc04629e3ddca24870dcc4b
YasamanMojaverian <yasaman.mojaverian@awacademy.training>
2022-10-19 15:09:30 +0200

    css wip

--- src/main/java/com/example/demo/DonationRepository.java
+++ src/main/java/com/example/demo/DonationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface DonationRepository extends CrudRepository<Donation, Long> {
-    List<Donation> findAllByOrganisation();
+
 }
